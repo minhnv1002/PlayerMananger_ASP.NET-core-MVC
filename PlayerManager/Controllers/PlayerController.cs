1d88b69436aeba134ca137fd1b12452bba772b1f
Van Minh <minhnv10201@gmail.com>
2024-11-08 10:31:12 +0700

    add 2 https

---
+++ PlayerManager/Controllers/PlayerController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PlayerManager.Data;
+using PlayerManager.Models;
+using PlayerManager.Models.Entities;
+
+namespace PlayerManager.Controllers
+{
+    public class PlayerController : Controller
+    {
+        private readonly ApplicationDbContext dbContext;
+
+        public PlayerController(ApplicationDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Add(AddPlayerViewModel viewModel)
+        {
+            var player = new Player
+            {
+                playerName = viewModel.playerName,
+                Email = viewModel.Email,
+                PhoneNumber = viewModel.PhoneNumber,
+                Contact = viewModel.Contact
+            };
+            await dbContext.players.AddAsync(player);
+            await dbContext.SaveChangesAsync();
+            return View();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> List()
+        {
+            var player = await dbContext.players.ToListAsync();
+            return View(player);
+        }
+    }
+}
---
+++ PlayerManager/Models/AddPlayerViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace PlayerManager.Models
+{
+    public class AddPlayerViewModel
+    {
+        public Guid Id { get; set; }
+        public string playerName { get; set; } = String.Empty;
+
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public bool Contact { get; set; }
+    }
+}
